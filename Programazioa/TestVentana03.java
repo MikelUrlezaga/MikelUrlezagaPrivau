53c8e728735c99b8189d949007e4adf48e4b0e11
MikelUrlezaga <murlezagap22@fpsanjorge.com>
2023-03-15 20:59:41 +0100

    werfw

--- Programazioa/TestVentana03.java
+++ Programazioa/TestVentana03.java
@@ -2,7 +2,6 @@ public class TestVentana03
 {
     public static void main (String[]args) {
         Ventana03 uno = new Ventana03( "Ventana 01" );
-        Ventana03 dos = new Ventana03( "Ventana 02" );
-    }   
-}   
-   
+    }
+}
+
