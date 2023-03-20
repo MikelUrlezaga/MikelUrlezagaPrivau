6ae2f3824b2ec355deccf2460deb1b78b841ba39
MikelUrlezaga <murlezagap22@fpsanjorge.com>
2023-03-20 14:22:18 +0100

    asfdas

---
+++ Programazioa/AdibLorezaintza/DB.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DB {
+
+   public static void main (String [] args){
+      Connection conn = null;
+      Statement stnt = null;
+      ResultSet rs = null;
+      String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+      String user = "LOREZAINTZA";
+      String pass = "LOREZAINTZA";
+      try {
+          conn = DriverManager.getConnection(url, user, pass);
+          stnt = conn.createStatement();
+          rs = stnt.executeQuery("SELECT * FROM PRODUKTUAK");
+          while (rs.next()) {
+            System.out.println(rs.getString ("IZENA"));
+          }
+          conn.close();
+          stnt.close();
+          rs.close();
+      } catch (SQLException e) {
+          System.out.println("ERROREA: " + e);
+      }
+   }
+}
