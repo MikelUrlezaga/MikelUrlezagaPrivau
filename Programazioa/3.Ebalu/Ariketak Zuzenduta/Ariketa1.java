b3df69ff57e841de6ece9967f42caf4ed17a25ba
MikelUrlezaga <murlezagap22@fpsanjorge.com>
2023-03-27 20:36:59 +0200

    jiof

--- Programazioa/3.Ebalu/Ariketak Zuzenduta/Ariketa1.java
+++ Programazioa/3.Ebalu/Ariketak Zuzenduta/Ariketa1.java
@@ -5,6 +5,7 @@ public class Ariketa1 {
    public static void main (String [] args){
       JFrame jf = new JFrame("Ariketa1");
       JTextField jtf = new JTextField ("Idatzi...");
+      jtf.setSize(10,20);
       JButton jb = new JButton ("Klikatu");
       jb.addActionListener (e -> testua(jtf));
       JPanel jp = new JPanel();
@@ -12,11 +13,12 @@ public class Ariketa1 {
       jp.add(jtf);
       jf.getContentPane().add(jp);      
       jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      jf.setSize(200,100);
+      
+      jf.setSize(400,200);
       jf.setVisible(true);
    }
    
    public static void testua (JTextField jtf){
-      jtf.setText("Kaxo mundua");
+      jtf.setText("Kaixo mundua");
    }
 }
