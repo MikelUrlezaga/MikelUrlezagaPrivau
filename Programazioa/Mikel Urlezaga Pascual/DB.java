e4dc53c670282ed060487557929fcc60b57fb7c9
MikelUrlezaga <murlezagap22@fpsanjorge.com>
2023-05-11 11:47:11 +0200

    ASFSAF

---
+++ Programazioa/Mikel Urlezaga Pascual/DB.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+
+
+public class DB {
+
+   private String url;
+   private String user;
+   private String pass;
+   
+   //zuen datuekin bete behar duzue
+   public DB (){
+      this.url="";
+      this.user="";
+      this.pass="";
+   }
+   
+   //sortzailea parametroekin
+   public DB (String url, String user, String pass){
+      this.url=url;
+      this.user=user;
+      this.pass=pass;
+   }
+   
+   
+   //konexioa egin, bat generikoa balio izango duena konexio guztietarako
+   public Connection konexioa (){
+      Connection conn = null;
+      try{
+         conn = DriverManager.getConnection(url, user, pass);
+         return conn;
+      } catch (Exception e){
+         System.out.println("Konexio errorea: "+e);
+      }
+      return conn;
+   }
+
+   ///HEMEN kontsultak
+   
+   // 1   SELECT * FROM BEZEROAK
+   
+   // 2   SELECT ID_BEZERO FROM BEZEROAK
+   
+   // 3   UPDATE BEZEROAK SET TELEFONOA = ? WHERE BEZEROKODEA = ?
+}
---
+++ Programazioa/Mikel Urlezaga Pascual/JDLogin.java
@@ -0,0 +1,4 @@
+// HAUEK IZANGO DIRA ERABILTZAILEAK
+   //private String [] erabiltzaileak = {"ALBA","ASIER","SERGIO","JON","IZASKUN","MIKEL","IMANOL","ERIK","XABIER","NATALIA","ANDER"};
+   //private int [] pasahitzak = {111,222,333,444,555,666,777,888,999,000,101};
+  
