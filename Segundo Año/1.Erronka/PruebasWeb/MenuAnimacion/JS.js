20b8e22a934711e27ad027c126d0eed013011cb6
MikelUrlezaga <117282447+MikelUrlezaga@users.noreply.github.com>
2023-10-11 12:47:43 +0200

    gaoiugsd

---
+++ Segundo Año/1.Erronka/PruebasWeb/MenuAnimacion/JS.js
@@ -0,0 +1,6 @@
+// script.js
+const element = document.getElementById("animatedElement");
+
+element.addEventListener("click", function() {
+  element.style.animation = "moveAnimation 2s linear";
+});
