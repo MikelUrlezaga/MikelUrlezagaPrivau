b2fe3f0c796bd662d46b7ed492b762215925d165
mikel <UrlezagaMikel@gmail.com>
2023-11-25 15:39:08 +0100

    asdfbj

---
+++ Segundo Año/JavaScript/JS-Fetch/fetch.js
@@ -0,0 +1,35 @@
+document.addEventListener('load', llamada());
+    // URL del archivo JSON o de la API que contiene los datos del Pokemonç
+    function llamada() {
+        
+    
+    const apiUrl = 'fetch.json';
+
+    // Selecciona el elemento HTML donde se mostrarán los detalles del Pokemon
+    const pokemonDetailsContainer = document.getElementById('pokemon-details');
+
+    // Realiza la solicitud (fetch) al servidor
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Procesa los datos y crea el contenido HTML
+            const pokemon = data[0]; // Suponiendo que solo hay un Pokemon en el JSON
+            var nombre = pokemon.name.toUpperCase();
+
+            const pokemonHTML = `
+                <h2>${nombre}</h2>
+                <p>ID: ${pokemon.id}</p>
+                <p>Altura: ${pokemon.height} dm</p>
+                <p>Peso: ${pokemon.weight} hg</p>
+                <p>Color: ${pokemon.species.color.name}</p>
+                <img src="${pokemon.species.varieties[0].pokemon.url}.png" alt="${pokemon.name}">
+            `;
+
+            // Inserta el contenido HTML en el contenedor
+            pokemonDetailsContainer.innerHTML = pokemonHTML;
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos del Pokemon:', error);
+        });
+    };
+
