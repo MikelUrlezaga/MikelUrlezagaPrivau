d42db1716e762d5601174d56a4366b9d25b027d6
MikelUrlezaga <117282447+MikelUrlezaga@users.noreply.github.com>
2024-01-08 08:08:43 +0100

    sdfg

--- Segundo Año/Zerbitzari/Ariketak/Laravel/Ariketa6/app/Http/Controllers/benetan.php
+++ Segundo Año/Zerbitzari/Ariketak/Laravel/Ariketa6/app/Http/Controllers/benetan.php
@@ -8,14 +8,19 @@ use App\Models\benetakoa;
 class benetan extends Controller
 {
     //
-    public function erakutsi(){
-        $emaitza = benetakoa::all();
-        // return view('erakutsi_view',compact('emaitza'));
+    // public function erakutsi(){
+    //     $emaitza = benetakoa::all();
+    //     // return view('erakutsi_view',compact('emaitza'));
 
-    }
+    // }
 
     public function index(){
         $emaitza = benetakoa::all();
+        return response()->json($emaitza);
+    }
+
+    public function erakutsi($id){
+        $emaitza = benetakoa::find($id);
         return response()->json($emaitza);
     }
 }
--- Segundo Año/Zerbitzari/Ariketak/Laravel/Ariketa6/routes/api.php
+++ Segundo Año/Zerbitzari/Ariketak/Laravel/Ariketa6/routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('erakutsi','App\Http\Controllers\benetan@index');
+
+Route::get('kaixo/{id}','App\Http\Controllers\benetan@erakutsi');
